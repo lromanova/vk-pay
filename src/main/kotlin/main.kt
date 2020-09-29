5ddf6addf45497b28ac1a15bdc666a0389fbc9d0
lromanova <perfectzvekla@gmail.com>
2020-09-29 21:09:09 +0300

    Добавлен Random

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,6 @@
 fun main() {
-    val amount: Int = 173000;
+    val amount: Int = (1..15000000).random()
+    println("Сумма перевода $amount копеек")
 
     val peny = amount * 0.0075
     val result = if(peny < 35) 35 else kotlin.math.round(peny)
