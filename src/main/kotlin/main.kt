63bd3ff731494d1becc9c728dc43d1cb38302c23
lromanova <perfectzvekla@gmail.com>
2020-10-01 17:10:29 +0300

    Исправлена ошибка - раньше сумма в копейках сравнивалась с минимальной комиссией в рублях. Теперь все в копейках

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,9 @@
 fun main() {
     val amount: Int = (1..15000000).random()
+    val min_peny: Int = 3500
     println("Сумма перевода $amount копеек")
 
     val peny = amount * 0.0075
-    val result = if(peny < 35) 35 else kotlin.math.round(peny)
+    val result = if(peny < min_peny) min_peny else kotlin.math.round(peny)
     println("Комиссия за перевод составит $result копеек")
 }
